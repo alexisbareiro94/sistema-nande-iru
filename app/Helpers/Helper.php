c0508ab51904f03cd7a2c3c52957f677f01f289a
Alexis Bareiro <alexisblugo@gmail.com>
2025-09-05 09:35:26 -0300

    feat: se agregra un helper global

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -2,8 +2,22 @@
 
 use Carbon\Carbon;
 
-if (! function_exists('format_time')) {
-    function format_time($time) {
+if (!function_exists('format_time')) {
+    function format_time($time)
+    {
         return Carbon::parse($time)->format('H:i - d/m/Y');
     }
+}
+
+function generate_code()
+{
+    $string = 'abcdefghijklmnopqrstxywzv123456789';
+    $largo = 10;
+    $array = str_split($string);
+    $code = '';
+    for ($i = 0; $i < $largo; $i++) {
+        $index = random_int(0, count($array) - 1);
+        $code .= $array[$index];
+    }
+    return $code;
 }
