663a7e0978de4a64161fd791dde6eae7dc06d1a5
Alexis Bareiro <alexisblugo@gmail.com>
2025-09-09 22:31:34 -0300

    fix: se corrige la vista, no mostraba los egresos e ingresos, solo las ventas, con la correccion de esto, tuve que separar las tablas de las ventas y movimientos, lo mas rapido que pense fue eso xd

--- resources/views/caja/historial-completo/index.blade.php
+++ resources/views/caja/historial-completo/index.blade.php
@@ -8,7 +8,7 @@
     <div class=" w-full mx-auto px-4 sm:px-6 lg:px-8 py-6 bg-gray-200  rounded-md min-h-screen">
         <div class="flex flex-col md:flex-row md:items-center md:justify-between mb-6 gap-4 p-6">
             <div>
-                <h2 class="text-2xl font-bold text-gray-800">Historial de ventas</h2>
+                <h2 class="text-2xl font-bold text-gray-800">Historial de Movimientos</h2>
                 <p class="text-gray-600 text-sm">Administra tu inventario de productos y servicios</p>
             </div>
             <a href="{{ route('producto.add') }}"
@@ -57,7 +57,7 @@
                         </i>
                     </div>
                     <div class="ml-4">
-                        <p class="text-sm font-medium text-gray-600">Ingresos</p>
+                        <p class="text-sm font-medium text-gray-600">Ingresos por venta</p>
                         <p class="text-2xl font-semibold text-gray-900">Gs. {{ number_format($ingresos, 0, ',', '.') }}</p>
                     </div>
                 </div>
@@ -106,21 +106,33 @@
         <!-- Filtros -->
         <div class="bg-white rounded-lg shadow mb-6">
             <div class="p-6">
-                <h3 class="text-lg font-medium text-gray-900 mb-4">Filtros de Búsqueda</h3>
-                <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+                <div class="flex justify-between">
+                    <h3 class="text-lg font-medium text-gray-900 mb-4">Filtros de Búsqueda</h3>
+
+                    <span id="dv-borrar-filtros" title="Borrar filtros" class="hidden group cursor-pointer">
+                        <svg class="w-[24px] h-[24px]  group-hover:text-white group-hover:bg-gray-800 rounded-full  text-gray-800 "
+                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2"
+                                d="M14.5 8.046H11V6.119c0-.921-.9-1.446-1.524-.894l-5.108 4.49a1.2 1.2 0 0 0 0 1.739l5.108 4.49c.624.556 1.524.027 1.524-.893v-1.928h2a3.023 3.023 0 0 1 3 3.046V19a5.593 5.593 0 0 0-1.5-10.954Z" />
+                        </svg>
+
+                    </span>
+                </div>
+                <div class="grid grid-cols-1 md:grid-cols-5 gap-4">
                     <div>
                         <label class="block text-sm font-medium text-gray-700 mb-1">Fecha Desde</label>
-                        <input type="date"
+                        <input id="dv-desde" type="date"
                             class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700 mb-1">Fecha Hasta</label>
-                        <input type="date"
+                        <input id="dv-hasta" type="date"
                             class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                     </div>
                     <div>
                         <label class="block text-sm font-medium text-gray-700 mb-1">Estado</label>
-                        <select
+                        <select id="dv-i-estado"
                             class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                             <option value="">Todos</option>
                             <option value="completado">Completado</option>
@@ -128,8 +140,19 @@
                             <option value="cancelado">Cancelado</option>
                         </select>
                     </div>
+
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700 mb-1">Forma de pago</label>
+                        <select id="dv-forma-pago"
+                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                            <option value="">Todos</option>
+                            <option value="transferencia">Transferencia</option>
+                            <option value="efectivo">Efectivo</option>
+                            <option value="mixto">Mixto</option>
+                        </select>
+                    </div>
                     <div class="flex items-end">
-                        <button
+                        <button id="dv-buscar"
                             class="w-full bg-gray-800 hover:bg-gray-800 cursor-pointer text-white px-4 py-2 rounded-md transition-colors">
                             <i class="fas fa-search mr-2"></i>
                             Buscar
@@ -153,10 +176,10 @@
                         </svg>
 
                     </span>
-                    <input class="min-w-sm border border-gray-400 pl-11 py-1 rounded-md"
-                        placeholder="Ingrese codigo de venta o cliente" type="text" name="" id="">
+                    <input id="dv-input-s" class="min-w-sm border border-gray-400 pl-11 py-1 rounded-md"
+                        placeholder="Ingrese codigo de venta o cliente" type="text" name="">
 
-                        {{-- feat: agergar funcion de exportar a pdf y excel --}}
+                    {{-- prox feat: agergar funcion de exportar a pdf y excel con o sin filtros --}}
                 </div>
             </div>
 
@@ -165,9 +188,10 @@
                     <thead class="bg-gray-50">
                         <tr>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                ID
+                                tipo
                             </th>
-                            <th class="flex px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th
+                                class="flex px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Fecha
                                 @include('productos.icons.nombres')
                             </th>
@@ -175,7 +199,7 @@
                                 Cliente
                             </th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Productos
+                                Productos/concepto
                             </th>
                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Total
@@ -188,87 +212,26 @@
                             </th>
                         </tr>
                     </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach ($ventas as $venta)                                                    
-                        <tr class="hover:bg-gray-50 transition-colors">
-                            <td class="venta-id px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">#{{ $venta->codigo }}</td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                {{ format_time($venta->created_at) }}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                {{ $venta->cliente->name ?? $venta->cliente->razon_social }}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                {{$venta->cantidad_productos}} productos
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                Gs. {{ number_format($venta->total,0,',','.') }}
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                {{-- class=" bg-yellow-100 text-yellow-800" --}}
-                                <span
-                                @class([
-                                    'px-2 inline-flex text-xs leading-5 font-semibold rounded-full',
-                                    'bg-yellow-100 text-yellow-800' => $venta->estado === 'pendiente',
-                                    'bg-green-100 text-green-800' => $venta->estado === 'completado',
-                                    'bg-red-100 text-red-800' => $venta->estado === 'cancelado',
-                                    ])>
-                                    {{ $venta->estado }}
-                                </span>
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                <div class="flex space-x-2">
-                                    <button id="btn-detalle-venta" data-ventaId="{{ $venta->codigo }}" class="detalle-venta cursor-pointer text-blue-600 hover:text-blue-900" title="Ver detalles">
-                                        <i class="fas fa-eye">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                                fill="currentColor" class="size-5">
-                                                <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                                                <path fill-rule="evenodd"
-                                                    d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </i>
-                                    </button>
-                                    <button class="text-green-600 cursor-pointer hover:text-green-900" title="Imprimir">
-                                        <i class="fas fa-print">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                                fill="currentColor" class="size-6">
-                                                <path fill-rule="evenodd"
-                                                    d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 0 0 3 3h.27l-.155 1.705A1.875 1.875 0 0 0 7.232 22.5h9.536a1.875 1.875 0 0 0 1.867-2.045l-.155-1.705h.27a3 3 0 0 0 3-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0 0 18 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM16.5 6.205v-2.83A.375.375 0 0 0 16.125 3h-8.25a.375.375 0 0 0-.375.375v2.83a49.353 49.353 0 0 1 9 0Zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 0 1-.374.409H7.232a.375.375 0 0 1-.374-.409l.526-5.784a.373.373 0 0 1 .333-.337 41.741 41.741 0 0 1 8.566 0Zm.967-3.97a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H18a.75.75 0 0 1-.75-.75V10.5ZM15 9.75a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V10.5a.75.75 0 0 0-.75-.75H15Z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-
-
-                                        </i>
-                                    </button>
-                                    <button class="text-red-600 hover:text-red-900 cursor-pointer" title="Eliminar">
-                                        <i class="fas fa-trash">
-
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                                fill="currentColor" class="size-5">
-                                                <path fill-rule="evenodd"
-                                                    d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-
-                                        </i>
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
+                    <tbody id="dv-body-tabla" class="bg-white divide-y divide-gray-200">
+                        @foreach ($ventas as $venta)                        
+                            @if ($venta->venta != null)
+                                @include('caja.historial-completo.includes.venta')
+                            @else
+                                @include('caja.historial-completo.includes.movimiento')
+                            @endif
+                        @endforeach
                     </tbody>
                 </table>
             </div>
 
             <!-- Paginación -->
-            <div class="min-w-full px-6 py-4 bg-white border-t border-gray-200 ">                        
-               {{$ventas->links()}}            
+            <div class="min-w-full px-6 py-4 bg-white border-t border-gray-200 ">
+                {{ $ventas->links() }}
             </div>
         </div>
     </div>
-    @section('js')
-        <script src="{{ asset('js/historial-ventas.js') }}"></script>
-    @endsection
+@section('js')
+    <script src="{{ asset('js/historial-ventas.js') }}"></script>
+@endsection
 @include('caja.historial-completo.detalle-venta')
 @endsection
