7c25c4f27be25b5602c9d8c269d5e6bad0353a60
Alexis Bareiro <alexisblugo@gmail.com>
2025-09-11 12:53:43 -0300

    fix: solucion al problema cuando un movimiento no tenia venta

--- public/js/historial-ventas.js
+++ public/js/historial-ventas.js
@@ -2,11 +2,9 @@ const modal = document.getElementById('modal-detalle-venta');
 function abrirModalDetalles() {
     const btnsdetalleVentas = document.querySelectorAll('.detalle-venta');
     btnsdetalleVentas.forEach(btn => {
-        btn.addEventListener('click', async () => {
-            const codigo = btn.dataset.ventaid;
-
+        btn.addEventListener('click', async () => {                
+            const codigo = btn.dataset.ventaid ?? btn.dataset.ventam;                     
             await detalleVentas(codigo);
-
             modal.classList.remove('hidden');
             setTimeout(() => {
                 modal.classList.remove('opacity-0');
@@ -15,7 +13,9 @@ function abrirModalDetalles() {
         })
     })
 }
+
 abrirModalDetalles();
+
 function cerrarModalDetalle() {
     modal.classList.remove('opacity-100');
     modal.classList.add('opacity-0');
@@ -33,7 +33,6 @@ document.getElementById('modal-detalle-venta').addEventListener('click', functio
     }
 });
 
-
 async function detalleVentas(codigo) {
     try {
         const res = await fetch(`http://localhost:8080/venta/${decodeURIComponent(codigo)}`, {
@@ -45,13 +44,15 @@ async function detalleVentas(codigo) {
         const data = await res.json();
         if (!res.ok) {
             throw data;
-        }
-
+        }               
+        
         setDataDetalleVenta(data);
-    } catch (err) {        
+    } catch (err) { 
+        console.log(err)       
         showToast(`${err.error}`, 'error')
     }
 }
+
 
 
 function setDataDetalleVenta(data) {
@@ -60,6 +61,10 @@ function setDataDetalleVenta(data) {
     const mPago = document.getElementById('d-v-pago');
     const codigo = document.getElementById('d-v-codigo');
     const total = document.getElementById('d-v-total');
+    const dvCajero = document.getElementById('dv-cajero');
+
+    dvCajero.innerText = `${data.venta.caja.user.name}`
+
     let metodoDePago = '';
     let estadoClass = data.venta.estado === 'completado' ? 'px-2 py-1 bg-green-100 text-green-800 rounded-full text-sm font-medium' :
         (data.venta.estado === 'cancelado' ? 'px-2 py-1 bg-red-100 text-red-800 rounded-full text-sm font-medium' :
@@ -82,27 +87,29 @@ function setDataDetalleVenta(data) {
     estado.classList = estadoClass;
     estado.innerText = data.venta.estado
 
-    if (data.venta.pagos.length === 2) {
-        metodoDePago = 'mixto'
-        document.getElementById('svg-mixto').classList.remove('hidden');
-        setMetodosPagosMixto(data);
-
-    } else {
-        if (!document.getElementById('svg-mixto').classList.contains('hidden')) {
-            document.getElementById('svg-mixto').classList.add('hidden');
+    if(data.productos != ''){        
+        if (data.venta.pagos.length === 2) {
+            metodoDePago = 'mixto'
+            document.getElementById('svg-mixto').classList.remove('hidden');
+            setMetodosPagosMixto(data);
+            
+        } else {
+            if (!document.getElementById('svg-mixto').classList.contains('hidden')) {
+                document.getElementById('svg-mixto').classList.add('hidden');
+            }
+            metodoDePago = data.venta.pagos[0].metodo;
         }
-        metodoDePago = data.venta.pagos[0].metodo;
-    }
-    mPago.innerText = metodoDePago;
-
-    if (data.venta.con_descuento) {
-        document.getElementById('dv-descuento').classList.remove('hidden');
-        document.getElementById('dv-subtotal').innerText = 'Gs. -' + data.venta.monto_descuento.toLocaleString('es-PY');
-
-    }
-    if (!data.venta.con_descuento) {
-        if (!document.getElementById('dv-descuento').classList.contains('hidden')) {
-            document.getElementById('dv-descuento').classList.add('hidden');
+        mPago.innerText = metodoDePago;
+        
+        if (data.venta.con_descuento) {
+            document.getElementById('dv-descuento').classList.remove('hidden');
+            document.getElementById('dv-subtotal').innerText = 'Gs. -' + data.venta.monto_descuento.toLocaleString('es-PY');
+            
+        }
+        if (!data.venta.con_descuento) {
+            if (!document.getElementById('dv-descuento').classList.contains('hidden')) {
+                document.getElementById('dv-descuento').classList.add('hidden');
+            }
         }
     }
 
@@ -111,7 +118,11 @@ function setDataDetalleVenta(data) {
     //productos
     setProductos(data);
     //total gral
-    total.innerText = 'Gs ' + data.venta.total.toLocaleString('es-PY');
+    if(data.productos == ''){
+        total.innerText = 'Gs ' + data.venta.monto.toLocaleString('es-PY');
+    }else{
+        total.innerText = 'Gs ' + data.venta.total.toLocaleString('es-PY');
+    }  
 }
 
 document.getElementById('svg-mixto').addEventListener('click', (e) => {
@@ -131,40 +142,46 @@ function setMetodosPagosMixto(data) {
 }
 
 function setCliente(data) {
-    const razon = document.getElementById('d-v-razon');
-    const rucCi = document.getElementById('d-v-ruc');
-
-    razon.innerText = data.venta.cliente.razon_social;
-    rucCi.innerText = data.venta.cliente.ruc_ci;
+    if(data.productos != ''){
+        const razon = document.getElementById('d-v-razon');
+        const rucCi = document.getElementById('d-v-ruc');
+        
+        razon.innerText = data.venta.cliente.razon_social;
+        rucCi.innerText = data.venta.cliente.ruc_ci;
+    }else{
+        return;
+    }
 }
 
 function setProductos(data) {
-    const bodyTabla = document.getElementById('d-v-bodyTable');
-    //const productos = {};
-    bodyTabla.innerHTML = '';
-    
-
-    data.productos.forEach(producto => {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-                                <td class="px-4 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
-                                    ${producto.nombre}
-                                </td>
-                                <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">
-                                    ${producto.codigo ?? 'sin codigo'}
-                                </td>
-                                <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">
-                                    ${producto.detalles[0].cantidad}
-                                </td>
-                                <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">
-                                    ${producto.detalles[0].producto_con_descuento ? producto.detalles[0].precio_descuento.toLocaleString('es-PY') : producto.precio_venta.toLocaleString('es-PY')}
-                                </td>
-                                <td class="px-4 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
-                                    Gs. ${producto.detalles[0].producto_con_descuento ? (producto.detalles[0].precio_descuento * producto.detalles[0].cantidad).toLocaleString('es-PY') : (producto.precio_venta * producto.detalles[0].cantidad).toLocaleString('es-PY')}
-                                </td>
-        `
-        bodyTabla.appendChild(tr);
-    })
+    console.log(data)
+    const bodyTabla = document.getElementById('d-v-bodyTable');    
+    bodyTabla.innerHTML = '';    
+    if(data.productos != ''){
+        data.productos.forEach(producto => {                  
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+            <td class="px-4 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
+            ${producto.nombre}
+            </td>
+            <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">
+            ${producto.codigo ?? 'sin codigo'}
+            </td>
+            <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">
+            ${producto.detalles[0].cantidad}
+            </td>
+            <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">
+            ${producto.detalles[0].producto_con_descuento ? producto.detalles[0].precio_descuento.toLocaleString('es-PY') : producto.precio_venta.toLocaleString('es-PY')}
+            </td>
+            <td class="px-4 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
+            Gs. ${producto.detalles[0].producto_con_descuento ? (producto.detalles[0].precio_descuento * producto.detalles[0].cantidad).toLocaleString('es-PY') : (producto.precio_venta * producto.detalles[0].cantidad).toLocaleString('es-PY')}
+            </td>
+            `
+            bodyTabla.appendChild(tr);        
+        })
+    }else{
+        
+    }
 }
 
 
@@ -194,8 +211,7 @@ document.getElementById('dv-buscar').addEventListener('click', () => {
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-    let datos = JSON.parse(sessionStorage.getItem('datos'));
-    console.log(datos)
+    let datos = JSON.parse(sessionStorage.getItem('datos'));    
     if(datos != null){
         document.getElementById('dv-borrar-filtros').classList.remove('hidden')
     }
@@ -386,8 +402,8 @@ function recargarTablaHistorialVentas(data) {
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                                         <div class="flex space-x-2">
-                                            <button id="btn-detalle-venta" data-ventaId=""
-                                                class="detalle-venta cursor-pointer text-blue-600 hover:text-blue-900" title="Ver detalles">
+                                            <button id="btn-detalle-movimiento" data-ventaM="${venta.id}"
+                                                class="detalle-movimiento cursor-pointer text-blue-600 hover:text-blue-900" title="Ver detalles">
                                                 <i class="fas fa-eye">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
                                                         <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
@@ -423,5 +439,6 @@ function recargarTablaHistorialVentas(data) {
         }
     });
     document.getElementById('paginacion').innerHTML = '';
+    abrirmodalDmDetalles();
     abrirModalDetalles();
 }
