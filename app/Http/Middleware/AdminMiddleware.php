fcd6dbf95469f17c3bf5600e589bda6981f9ecf6
Alexis Bareiro <alexisblugo@gmail.com>
2025-10-10 10:52:55 -0300

    feat: mejora la gestión de acceso no autorizado en el middleware de administración

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Events\NotificacionEvent;
 
 class AdminMiddleware
 {
@@ -15,7 +16,13 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if($request->user()->role != 'admin'){
+        if ($request->user()->role != 'admin') {
+            $user = auth()->user();                  
+            NotificacionEvent::dispatch(
+                'ALERTA!',
+                "Intento de acceso no autorizado, por: " . ($user->name ?? $request->ip()) . " en " . $request->fullUrl(),
+                'red'
+            );
             return redirect()->route('login')->with('error', 'no tienes permisos');
         }
         return $next($request);
